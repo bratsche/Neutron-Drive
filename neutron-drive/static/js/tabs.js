ccff6c787718ae059af81b894e549ee7a97c6c69
Paul Bailey <paul.m.bailey@gmail.com>
2012-07-29 21:34:08 -0500

    fixes #3

--- neutron-drive/static/js/tabs.js
+++ neutron-drive/static/js/tabs.js
@@ -14,25 +14,38 @@ Tabs.add_file = function (file_id, name, mime, session) {
     saved_once: false
   };
   
+  var l = (Tabs.files.length - 1) * 130;
   $("#tab_bar span.current").removeClass('current');
-  var html = '<span class="current" id="' + file_id + '">';
-  html = html + '<a href="javascript: void(0)">' + name + '</a>';
+  var html = '<span class="current" id="' + file_id + '" style="left: ' + l + 'px;">';
+  html = html + '<a href="javascript: void(0)" title="' + name + '">' + name + '</a>';
   html = html + '<button type="button" class="close">×</button>';
   
   $("#tab_bar").append(html);
-  $("#tab_bar span#" + file_id + ' a').click(function () { Tabs.switch_tab(file_id); });
+  $("#tab_bar span#" + file_id + ' a').click(function () { Tabs.switch_tab(file_id, true); });
   $("#tab_bar span#" + file_id + ' button').click(function () { Tabs.remove_tab(file_id); });
+  
+  $("#tab_bar").animate({scrollLeft: l}, 500);
+  
+  var html2 = '<li><a id="tabsel_' + file_id + '" href="javascript: void(0);" onclick="Tabs.switch_tab(\'' + file_id + '\')">' + name + '</a></li>';
+  $("#tab_drop").append(html2);
 };
 
-Tabs.switch_tab = function (file_id) {
+Tabs.switch_tab = function (file_id, noscroll) {
   $("#tab_bar span.current").removeClass('current');
   $("#tab_bar span#" + file_id).addClass('current');
   Editor.setSession(Tabs.data[file_id].session);
+  
+  if (noscroll) {}
+  else {
+    var l = Tabs.files.indexOf(file_id) * 130;
+    $("#tab_bar").animate({scrollLeft: l}, 500);
+  }
 };
 
 Tabs.remove_tab = function (file_id) {
   var f = Tabs.files.indexOf(file_id);
   var sel = "#tab_bar span#" + file_id;
+  var tab_switch = false;
   
   if (Tabs.files.length > 1) {
     if ($(sel).hasClass('current')) {
@@ -41,7 +54,7 @@ Tabs.remove_tab = function (file_id) {
         c = f - 1;
       }
       
-      Tabs.switch_tab(Tabs.files[c]);
+      tab_switch = Tabs.files[c];
     }
   }
   
@@ -55,4 +68,13 @@ Tabs.remove_tab = function (file_id) {
   Tabs.files.splice(f, 1);
   
   $(sel).remove();
+  $('#tabsel_' + file_id).remove();
+  for (i in Tabs.files) {
+    var l = i * 130;
+    $("#" + Tabs.files[i]).css('left', l + 'px');
+  }
+  
+  if (tab_switch) {
+    Tabs.switch_tab(tab_switch);
+  }
 };
